f3c6de345c07975d9e426c0c68399d5afcfeaf8f
nikhilk <nikhilk@live.com>
2008-12-20 22:01:26 -0800

    Add core SilverlightFX project to solution

---
+++ src/Client/Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿// AssemblyInfo.cs
+// Copyright (c) Nikhil Kothari, 2008. All Rights Reserved.
+// http://www.nikhilk.net
+//
+// Silverlight.FX is an application framework for building RIAs with Silverlight.
+// This project is licensed under the BSD license. See the accompanying License.txt
+// file for more information.
+// For updated project information please visit http://projects.nikhilk.net/SilverlightFX.
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle("SilverlightFX")]
+[assembly: AssemblyDescription("Silverlight.FX Core Framework and Platform Extensions")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://projects.nikhilk.net")]
+[assembly: AssemblyProduct("Silverlight.FX")]
+[assembly: AssemblyCopyright("Copyright © 2008, Nikhil Kothari. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
