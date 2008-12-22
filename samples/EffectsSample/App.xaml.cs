f9b7c5d2cc0dec4336336bd227d6cdb14716d83f
nikhilk <nikhilk@live.com>
2008-12-22 06:57:10 -0800

    Sample for declarative effects and transitions

---
+++ samples/EffectsSample/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿// App.xaml.cs
+//
+
+using System;
+using System.Windows;
+using SilverlightFX.Applications;
+
+namespace EffectsSample {
+
+    public partial class App : XApplication {
+
+        public App() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ samples/EffectsSample/Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿// Page.xaml.cs
+//
+
+using System;
+using System.Windows.Controls;
+
+namespace EffectsSample {
+
+    public partial class Page : UserControl {
+
+        public Page() {
+            InitializeComponent();
+        }
+    }
+}
