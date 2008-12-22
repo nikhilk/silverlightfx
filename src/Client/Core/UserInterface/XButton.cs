8877e3fc3af21e8927c13eba717fb0292a51c94d
nikhilk <nikhilk@live.com>
2008-12-21 16:17:21 -0800

    Extended button control

---
+++ src/Client/Core/UserInterface/XButton.cs
@@ -0,0 +1,33 @@
+﻿// XButton.cs
+// Copyright (c) Nikhil Kothari, 2008. All Rights Reserved.
+// http://www.nikhilk.net
+//
+// This product's copyrights are licensed under the Creative
+// Commons Attribution-ShareAlike (version 2.5).B
+// http://creativecommons.org/licenses/by-sa/2.5/
+//
+// Silverlight.FX is an application framework for building RIAs with Silverlight.
+// This project is licensed under the BSD license. See the accompanying License.txt
+// file for more information.
+// For updated project information please visit http://projects.nikhilk.net/SilverlightFX.
+//
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SilverlightFX.UserInterface {
+
+    /// <summary>
+    /// An extended Button control with more functionality.
+    /// </summary>
+    public class XButton : Button {
+
+        /// <summary>
+        /// Allows invoking the associated Click event handler programmatically.
+        /// </summary>
+        public void PerformClick() {
+            OnClick();
+        }
+    }
+}
