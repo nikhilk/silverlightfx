e1f887337bd79ff7b89527466ef30b8c336b8b9e
nikhilk <nikhilk@live.com>
2009-01-07 23:48:04 -0800

    Fix file header comment.

--- src/Client/Core/UserInterface/AutoComplete.cs
+++ src/Client/Core/UserInterface/AutoComplete.cs
@@ -2,10 +2,6 @@
 // Copyright (c) Nikhil Kothari, 2008. All Rights Reserved.
 // http://www.nikhilk.net
 //
-// This product's copyrights are licensed under the Creative
-// Commons Attribution-ShareAlike (version 2.5).B
-// http://creativecommons.org/licenses/by-sa/2.5/
-//
 // Silverlight.FX is an application framework for building RIAs with Silverlight.
 // This project is licensed under the BSD license. See the accompanying License.txt
 // file for more information.
