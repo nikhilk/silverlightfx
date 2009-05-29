9cae565ad0b3e4bc3c91327963bcb3428443ff87
NikhilK <nikhilk@live.com>
2009-05-29 00:55:18 -0700

    Update News sample with slightly more interesting view model

--- samples/News/Views/News/ListPage.Model.cs
+++ samples/News/Views/News/ListPage.Model.cs
@@ -4,15 +4,38 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using NewsWidget.Data;
 
 namespace NewsWidget.Views.News {
 
     public class ListPageModel : Model {
 
+        private bool _filterToday;
+
+        public bool FilterToday {
+            get {
+                return _filterToday;
+            }
+            set {
+                _filterToday = value;
+                RaisePropertyChanged("FilterToday", "ListItems");
+            }
+        }
+
         public IEnumerable<NewsItem> Items {
             get;
             set;
+        }
+
+        public IEnumerable<NewsItem> ListItems {
+            get {
+                if (_filterToday == false) {
+                    return Items;
+                }
+
+                return Items.Where(item => item.PublishDate.Date == DateTime.UtcNow.Date);
+            }
         }
     }
 }
